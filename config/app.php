9de6a72c83e5ee8aa4b4aee754f6c8074bf2ce12
Brian Smith <info@creativestudio.co.za>
2017-11-10 14:20:03 +0200

    added the vendor mail templates for auth

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Backend'),
+    'name' => env('APP_NAME', 'bceReferralPortal'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://referral.dev'),
 
     /*
     |--------------------------------------------------------------------------
